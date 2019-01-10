5917a753ee5c108f0e62a4a7c6c843e53a74d124
Tony Gilkerson <tonygilkerson@yahoo.com>
2019-01-10 13:18:29 -0500

    might need the namespace

--- vars/k8sFuncTestGolang.groovy
+++ vars/k8sFuncTestGolang.groovy
@@ -2,8 +2,7 @@ def call(project, domain) {
     //addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
     //127.0.0.1.nip.io does not work in a container so I had to do this
     //to get it to work locally
-    addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}:8080"
-    chartName = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}"
+    addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${project}-build:8080"
     sh "go get -d -v -t"
     sh """ADDRESS=${addr} \
         go test ./... -v \

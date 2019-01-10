762d7725601e7611a433cd58f1b59844148e2b7c
Tony Gilkerson <tonygilkerson@yahoo.com>
2019-01-10 12:28:49 -0500

    modify to not use nip.io

--- vars/k8sFuncTestGolang.groovy
+++ vars/k8sFuncTestGolang.groovy
@@ -1,5 +1,9 @@
 def call(project, domain) {
-    addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
+    //addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
+    //127.0.0.1.nip.io does not work in a container so I had to do this
+    //to get it to work locally
+    addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}:8080"
+    chartName = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}"
     sh "go get -d -v -t"
     sh """ADDRESS=${addr} \
         go test ./... -v \
